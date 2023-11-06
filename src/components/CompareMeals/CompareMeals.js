402020f132e8744b488bb19bd9b26a770a7e182b
kitpao <kitpao91@gmail.com>
2023-11-06 05:48:28 -0600

    sketch in comments the comparisson idea

--- src/components/CompareMeals/CompareMeals.js
+++ src/components/CompareMeals/CompareMeals.js
@@ -6,9 +6,21 @@ class CompareMeals extends React.Component {
     return (
       <section className="compare-meals">
         THIS IS comparing
-        { /* Left Side Component for meal 1 */ }
-        { /* Right Side Component for meal 2 */ }
-        { /* remember to change css dynamically depending on comparison result */ }
+        { /* Left Side Component for first meal with dropdown selector, reuse the meal description component inside */ } 
+        { /* Right Side Component for second meal with dropdown selector, reuse the meal description component inside */ }
+        { /* remember to change css dynamically highlighting the best option depending on comparison result */ }
+        { /*
+        
+          idea was to highlight the best attributes of each meal and the provide a conclusion for the scenario
+          example:
+
+            meal 1                     |  meal 2                    | Conclusion
+            cooking time 1: 20 minutes | cooking time 2: 85 minutes | If you care about cooking fast: meal 1
+            caloriesPer100gr: 200kcal  | caloriesPer100gr: 110kcal  | If you promote healthy food: meal 2
+            ingredients: ...           | ingredients: ...           | If you want to be practical (less ing): meal 1
+
+            Total pros: 4              | Total pros: 2              | In general, meal 1 is the best
+        */ }
       </section>
     );
   }
